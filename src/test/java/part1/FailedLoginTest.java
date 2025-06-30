18fde1afd505f76507d896bf96eef82fd7f408f9
samcodeonline <115877996+samcodeonline@users.noreply.github.com>
2025-06-30 05:37:39 +0500

    updated

---
+++ src/test/java/part1/FailedLoginTest.java
@@ -0,0 +1,45 @@
+package part1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FailedLoginTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void SetUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    }
+
+    @AfterClass
+    public void TearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void testLogginIntoApplication() throws InterruptedException{
+        Thread.sleep(2000);
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("Admin");
+        var password = driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+
+        driver.findElement(By.tagName("button")).click();
+
+        Thread.sleep(2000);
+        String actualResult = driver.findElement(By.tagName("h6")).getText();
+        String expectedResult = "Dashboard";
+
+        Assert.assertNotEquals(actualResult, expectedResult);
+    }
+}
